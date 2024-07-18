645954ffc1fd1d4fb64813139d9a44061ef7f6b4
Lina <lindleeotaku@gmail.com>
2024-07-18 18:36:55 +0300

    done

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void Numbers(int M, int N)
+{
+    if (M > N)
+    {
+            return;
+    }
+    Console.WriteLine(M);
+    Numbers(M + 1, N);
+}
+
+Console.Write("Input M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+        
+ Console.Write("Input N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+        
+Numbers(M, N);
---
+++ 3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Input size of array");
+int size = Convert.ToInt32(System.Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size);
+
+PrintArray(myArray);
+PrintArrayRevers(myArray, myArray.Length - 1);
+
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+     for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 9 + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+            Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void PrintArrayRevers(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+
+        Console.Write(array[index] + " ");
+        PrintArrayRevers(array, index - 1);
+}
+
